6b5c6ffd587348ce9162d3fab9a88b190696304c
Kelly Lima <keely06@live.com>
2019-05-30 22:33:54 -0300

    mudança na array

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser')
 const controller = require('./ComidasController');
 
 
@@ -11,5 +12,11 @@ server.get('/comidas', (request, response) => {
     response.send(controller.getAll());
 })
 
-server.listen(3010)
+server.post('/comidas', bodyParser.json(), (request, response) => {
+    controller.add(request.body);
+    response.send(201);
+    // response.send(controller.add(request.body));
+})
+
+server.listen(3000)
 console.log('servidorzinho rodando na porta 3000'); //mostra no terminal
