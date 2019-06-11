7884a8eb747c7cac968b21c5f4d648dfbeef1196
Kelly Lima <keely06@live.com>
2019-06-10 22:28:20 -0300

    criacao de schema

--- ComidasRepository.js
+++ ComidasRepository.js
@@ -1,11 +1,17 @@
-const comidas = {
-    prato: [
-        {
-            nome: "Batata frita",
-            descricao: "Batatinha show",
-            id: "1234"
+const mongoose = require('mongoose');
+//string de conexao: mongodb://dominio:porta/nome_database
+
+const MONGO_URL = "mongodb://localhost:27017/reprograma";
+
+function connect() {
+    //TODO: conectar no nosso monngo usando a MONGO_URL
+    mongoose.connect(MONGO_URL, {userNewParser: true}, function(error) {
+        if(error) {
+            console.error('Deu erro: ', error);
+        } else {
+            console.log('Sucessoo!!')
         }
-    ]
+    });
 }
 
-module.exports = {comidas}
+module.exports = { connect }
---
+++ ComidasSchema.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+//cada schema equivale a collection
+const Schema = mongoose.Schema;
+const ComidasSchema = new Schema({
+    _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
+    nome: { type: String, required: true },
+    descricao: { type: String }
+})
+
+const comidasModel = mongoose.model('comidas', ComidasSchema);
+
+module.exports = { comidasModel };
